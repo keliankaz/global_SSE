824923fa12bd31478aea70be8575adc7910fc30a
keliankaz <kdascher@ucsc.edu>
2023-09-06 18:53:14 -0700

    init for arcs

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -2,4 +2,5 @@ from .catalog import Catalog
 from .earthquake import EarthquakeCatalog
 from .slowslip import SlowSlipCatalog
 from .slab2 import Slab, AllSlabs
+from .arcs import Arcs
 from .utils import Scaling, get_xyz_from_lonlat
